f46c48f0cdf4bf848c534136119e8eced589efb7
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-09-24 19:55:13 +0200

    Add Redux, enable routing functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ingredientsReducer from 'store/reducers/ingredientsReducer';
+import recipesReducer from 'store/reducers/recipesReducer';
+
+const rootReducer = combineReducers({
+    ingredients: ingredientsReducer,
+    recipes: recipesReducer
+});
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
